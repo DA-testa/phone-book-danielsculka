214b8810476a4615c1592a66d2190b43e58ebfca
Dāniels Čulka <daniels.culka@edu.rtu.lv>
2023-04-02 22:45:53 +0300

    init

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # python3
+# Dāniels Čulka 221RDB304
 
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
+
         if self.type == 'add':
             self.name = query[2]
 
@@ -16,23 +18,24 @@ def write_responses(result):
 
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
     contacts = []
+
     for cur_query in queries:
+        
         if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
             for contact in contacts:
                 if contact.number == cur_query.number:
                     contact.name = cur_query.name
                     break
-            else: # otherwise, just add it
+            else:
                 contacts.append(cur_query)
+
         elif cur_query.type == 'del':
             for j in range(len(contacts)):
                 if contacts[j].number == cur_query.number:
                     contacts.pop(j)
                     break
+
         else:
             response = 'not found'
             for contact in contacts:
@@ -40,6 +43,7 @@ def process_queries(queries):
                     response = contact.name
                     break
             result.append(response)
+
     return result
 
 if __name__ == '__main__':
